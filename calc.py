227a53ac684bb7fc187c02564c6cbe7e75ffe654
WalshM27 <johnst27@chcstudent.com>
2025-09-12 16:30:15 -0400

    calc

---
+++ calc.py
@@ -0,0 +1,13 @@
+numone = input("enter your first number ")
+numone = int(numone)
+numtwo = input("enter your second number ")
+numtwo = int(numtwo)
+sum = numone + numtwo
+print(f"the sum is {sum}")
+
+numthree= input("enter your first number ")
+numthree = int(numthree)
+numfour = input("enter your second number ")
+numfour = int(numfour)
+difference = numthree - numfour
+print(f"the difference is {difference}")
---
+++ variables2.py
@@ -0,0 +1,6 @@
+numsix = input("enter your first number ")
+numsix = int(numsix)
+djsigma = input("enter your second number ")
+djsigma = int(djsigma)
+sum = numsix + djsigma
+print(f"the sum is {sum}")
