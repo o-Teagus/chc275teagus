a3d33ca32926093651737ccf659e8f61e73c5adc
WalshM27 <johnst27@chcstudent.com>
2025-09-12 15:59:07 -0400

    commit

---
+++ variables.py
@@ -0,0 +1,16 @@
+x = 5 
+print(x)
+y = 10
+print (x + y)
+print (x - y)
+print (x*y)
+print (x/y)
+print (x**y)
+sum = x + y
+print (sum)
+sum=sum+20
+print(sum)
+name = "o teagus"
+print(name)
+print(f"my name is {name}")
+print (f"the sum of x and y is {sum}")
