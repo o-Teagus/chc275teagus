2a4e94ce82b74b654b31df9e834806fdab5b9089
WalshM27 <johnst27@chcstudent.com>
2025-09-15 15:45:22 -0400

    aV CDZSEW

---
+++ branching.py
@@ -0,0 +1,6 @@
+x = 15
+if x  % 2 ==0:
+    print(f"{x} is divisible by 2")
+elif x % 3 == 0:
+    print(f"{x} is divisible by 3")
+    print("example")
--- calc.py
+++ calc.py
@@ -1,13 +1,27 @@
-numone = input("enter your first number ")
-numone = int(numone)
-numtwo = input("enter your second number ")
-numtwo = int(numtwo)
-sum = numone + numtwo
+num1 = input("enter your first number ")
+num1 = int(num1)
+num2 = input("enter your second number ")
+num2 = int(num2)
+sum = num1 + num2
 print(f"the sum is {sum}")
 
-numthree= input("enter your first number ")
-numthree = int(numthree)
-numfour = input("enter your second number ")
-numfour = int(numfour)
-difference = numthree - numfour
+num3= input("enter your first number ")
+num3 = int(num3)
+num4 = input("enter your second number ")
+num4 = int(num4)
+difference = num3 - num4
 print(f"the difference is {difference}")
+
+num5 = input("enter your first number ")
+num5 = int(num5)
+num6 = input("enter your second number ")
+num6 = int(num6)
+product = num5 * num6
+print(f"the product is {product}")
+
+num7 = input("enter your first number ")
+num7 = int(num7)
+num8 = input("enter your second number ")
+num8 = int(num8)
+quotient = num7/num8
+print(f"the quotient is {quotient}")
