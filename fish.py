90a0e9b5d0b0a4c01d2d4bd269a7922fe4e858b8
WalshM27 <johnst27@chcstudent.com>
2025-09-23 15:45:25 -0400

    seis siete

---
+++ fish.py
@@ -0,0 +1,18 @@
+
+option = input("Enter your fish type:")
+if option == "carniviorous":
+    x=input("Do you already have it?:")
+    if x == "yes":
+        print("too bad")
+
+    elif x == "no":
+            print("enjiy")
+        
+elif option == "salt water":
+    x=input("youre a fancy fish parent")
+
+elif option == "community":
+    print("you should get more than one")
+    
+else:
+    print ("i dont think thats a kind of fish") 
---
+++ powerelevels.py
@@ -0,0 +1,15 @@
+
+
+x=input("num1")
+y=input("num2")
+x=int(x)
+y=int(y)
+
+if x > y:
+    print("num 1 wins")
+
+elif y > x:
+    print("num 2 wins")
+
+elif x == y:
+    print("it is a tie")
